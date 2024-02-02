70844947afee31e410bee56fea4fb2b70c85fab2
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-02-02 10:07:12 -0500

    use axios instead of supertest

--- test/int.test.js
+++ test/int.test.js
@@ -1,27 +1,29 @@
-const createServer = require('../src/server')
-const supertest = require('supertest')
+const axios = require('axios')
 
-const app = createServer()
-const request = supertest(app)
+const BASE_URL = 'http://localhost:3333'
 
 describe('/calc/add', () => {
   it('should return 5, because 2 + 3 = 5', async () => {
-    await request
-      .get('/calc/add')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        res: 5
-      })
+    const response = await axios.get(`${BASE_URL}/calc/add`)
+
+    expect(response.status).toEqual(200)
+    expect(response.statusText).toEqual('OK')
+
+    expect(response.data).toStrictEqual({
+      res: 5
+    })
   })
 })
 
 describe('/calc/subtract', () => {
   it('shuld return 3, because 5 -2 = 3', async () => {
-    await request
-      .get('/calc/subtract')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        res: 3
-      })
+    const response = await axios.get(`${BASE_URL}/calc/subtract`)
+
+    expect(response.status).toEqual(200)
+    expect(response.statusText).toEqual('OK')
+
+    expect(response.data).toStrictEqual({
+      res: 3
+    })
   })
 })
