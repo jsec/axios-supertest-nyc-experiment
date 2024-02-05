3e98f686c6e5bb80abeeb72fb8d663fdbd57770d
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-02-05 13:32:40 -0500

    use Jest assertions with supertest

--- test/int.test.js
+++ test/int.test.js
@@ -6,22 +6,24 @@ const request = supertest(app)
 
 describe('/calc/add', () => {
   it('should return 5, because 2 + 3 = 5', async () => {
-    await request
-      .get('/calc/add')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        res: 5
-      })
+    const { statusCode, type, body } = await request.get('/calc/add')
+
+    expect(statusCode).toEqual(200)
+    expect(type).toMatch(/json/)
+    expect(body).toStrictEqual({
+      res: 5
+    })
   })
 })
 
 describe('/calc/subtract', () => {
   it('shuld return 3, because 5 -2 = 3', async () => {
-    await request
-      .get('/calc/subtract')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        res: 3
-      })
+    const { statusCode, type, body } = await request.get('/calc/subtract')
+
+    expect(statusCode).toEqual(200)
+    expect(type).toMatch(/json/)
+    expect(body).toStrictEqual({
+      res: 3
+    })
   })
 })
